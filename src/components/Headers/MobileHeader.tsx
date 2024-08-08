c9134d6bf2160e167fceeda2d3ef5edc0f2c6ddd
david waza <davidyega69@gmail.com>
2024-08-08 02:09:10 +0100

    fix: hamburger toggle

--- src/components/Headers/MobileHeader.tsx
+++ src/components/Headers/MobileHeader.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
+import { Menu, X } from "lucide-react";
 
 const MobileNav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,22 +10,31 @@ const MobileNav = () => {
     <nav className="bg-white p-4">
       <div className="flex items-center justify-between flex-wrap">
         <div className="flex items-center flex-shrink-0 text-[#228634] px-6">
-          <span className="font-semibold text-xl tracking-tight">Ostat Aid</span>
+          <span className="font-semibold text-xl tracking-tight">
+            Ostat Aid
+          </span>
         </div>
         <div className="block lg:hidden">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="flex items-center px-3 py-2 border rounded text-gray-400 border-gray-400 hover:text-white hover:border-white"
-          >
-            <svg
-              className="fill-current h-5 w-5 text-[#228634]"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
+          {isOpen ? (
+            <>
+              {" "}
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center px-3 py-2 border rounded text-gray-400 border-gray-400 hover:text-white hover:border-white transition-all ease-in-out"
+              >
+                <X color="#228634" />
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center px-3 py-2 border rounded text-gray-400 border-gray-400 hover:text-white hover:border-white transition-all ease-in-out"
+              >
+                <Menu color="#228634" />
+              </button>
+            </>
+          )}
         </div>
         <div
           className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto px-6 ${
